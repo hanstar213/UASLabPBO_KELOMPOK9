218b123ea6f9c7e8ff452392743cd1cc61a6723d
Raysha Tazkiya Rahim <167387273+rayshazkya@users.noreply.github.com>
2024-11-24 17:48:33 +0700

    Update pembayaran.java

--- pembayaran.java
+++ pembayaran.java
@@ -7,3 +7,35 @@ public abstract class pembayaran {
    
     public abstract void prosesPembayaran();
 }
+
+
+// Subkelas Pembayaran
+public class QRIS extends Pembayaran {
+    public QRIS(String id) {
+        super(id);
+    }
+
+    @Override
+    public void prosesPembayaran() {
+    }
+}
+
+public class Bank extends Pembayaran {
+    public Bank(String id) {
+        super(id);
+    }
+
+    @Override
+    public void prosesPembayaran() {
+    }
+}
+
+public class COD extends Pembayaran {
+    public COD(String id) {
+        super(id);
+    }
+
+    @Override
+    public void prosesPembayaran() {
+    }
+}
