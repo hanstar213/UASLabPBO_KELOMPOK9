52afb64709df3a55196c2e231bc2a2a5aa152d2a
Raysha Tazkiya Rahim <167387273+rayshazkya@users.noreply.github.com>
2024-11-28 12:35:40 +0700

    Update pembayaran.java

--- pembayaran.java
+++ pembayaran.java
@@ -1,41 +1,48 @@
-public abstract class pembayaran {
-    protected String id;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
 
-    public pembayaran(String id) {
-        this.id = id;
-    }
-   
-    public abstract void prosesPembayaran();
-}
+public abstract class Pembayaran {
+    protected String idPembayaran;
+    private static final String FILE_NAME = "Transaksi.txt"; // Nama file tempat ID disimpan
 
-
-// Subkelas Pembayaran
-public class QRIS extends Pembayaran {
-    public QRIS(String id) {
-        super(id);
+    public Pembayaran(String idPembayaran) {
+        this.idPembayaran = idPembayaran;
     }
 
-    @Override
-    public void prosesPembayaran() {
-    }
-}
-
-public class Bank extends Pembayaran {
-    public Bank(String id) {
-        super(id);
+    protected String generateUniqueId() {
+        Random random = new Random();
+        return String.format("%03d", 100 + random.nextInt(900));  // Menghasilkan angka acak 3 digit
     }
 
-    @Override
-    public void prosesPembayaran() {
-    }
-}
+    // Metode abstrak untuk menghasilkan ID pembayaran
+    public abstract void generateIdPembayaran();
 
-public class COD extends Pembayaran {
-    public COD(String id) {
-        super(id);
+    public String getIdPembayaran() {
+        return idPembayaran;
     }
 
-    @Override
-    public void prosesPembayaran() {
+    // Memeriksa apakah ID sudah ada di dalam file
+    protected boolean isIdExist(String id) {
+        Set<String> existingIds = loadExistingIdFromFile(FILE_NAME);
+        return existingIds.contains(id);
+    }
+
+    // Memuat ID yang sudah ada dari file Pembayaran.txt
+    private Set<String> loadExistingIdFromFile(String fileName) {
+        Set<String> existingId = new HashSet<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length > 0) {
+                    existingId.add(parts[0].trim()); // Ambil ID pembayaran yang ada
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file.. " + e.getMessage());
+        }
+        return existingId;
     }
 }
