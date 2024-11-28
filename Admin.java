41d5d23a49b970a347a2303566edfa8c3a03fb5e
RahmadHidayatullah <166033080+Ryonn12@users.noreply.github.com>
2024-11-28 19:37:16 +0700

    update

--- Admin.java
+++
@@ -1,7 +0,0 @@
-public class Admin extends Account {
-
-    // Constructor untuk Admin
-    public Admin(String id, String userName, String password) {
-        super(id, userName, password); // Memanggil konstruktor dari superclass (Account)
-    }
-}
